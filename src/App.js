cecd902b51ee902fce65a1a97bae4decd77c818f
Ale Mota <203404@ids.upchiapas.edu.mx>
2021-11-28 23:21:06 -0600

    crudapi y demas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import CruddApp from './componentes/crudApp';
-import CrudForm from './componentes/crudForm';
-import CrudApi from './componentes/crudApi';
+import CruddApp from './componentes/CrudApp';
+import CrudForm from './componentes/Crudform';
+import CrudApi from './componentes/CrudApi';
 
 function App() {
   return (
