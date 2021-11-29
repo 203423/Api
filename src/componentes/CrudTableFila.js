206bc4cfe9f4bfabe21c15cebfdd601a992651ea
Ale Mota <203404@ids.upchiapas.edu.mx>
2021-11-29 10:48:58 -0600

    bda

--- src/componentes/CrudTableFila.js
+++ src/componentes/CrudTableFila.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 const CrudTableFila=({el, setDatoEditar, deleteRegistro})=>{
-    let{usuario, contrasenia, id}=el;
+    let{usuario, contrasenia, id, product, type}=el;
     return (
         <tr>
             <td>{usuario}</td>
             <td>{contrasenia}</td>
-             <td>
+            <td>{id}</td>
+            <td>{product}</td>
+            <td>{type}</td>
+
+            <td>
                 <button onClick={() => setDatoEditar(el) }>Editar</button>
                 <button onClick={() => deleteRegistro(id) }>Eliminar</button>
-             </td>
+            </td>
         </tr>
+
+
+        
     );
 };
 
