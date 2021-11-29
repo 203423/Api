877760c8ab8eeecb4d891f3ece402915d0b0aa57
Ale Mota <203404@ids.upchiapas.edu.mx>
2021-11-28 22:59:05 -0600

    a

--- src/componentes/CrudTable.js
+++ src/componentes/CrudTable.js
@@ -1,39 +1,36 @@
 import React from "react";
 import CrudTableFila from "./CrudTableFila";
 
-const CrudTable = ({data, setDatoEditar, deleteRegistro}) =>{
-   
-    return(
-        <div class="ctabla">
-            <table>
-                <thread>
-                    <tr>
-                        <th>Usuario</th>
-                        <th></th>
-                        <th>contraseña</th>
-                    </tr>
-                </thread>
-                <tbody>
-                    {data.length >0?
-
-                        (data.map((el)=> (<CrudTableFila 
-                            key={el.id} 
-                            el={el}
-                            setDatoEditar={setDatoEditar}
-                            deleteRegistro={deleteRegistro}
-                            />
-                            ))
-               
-                    ):(
-
-                        <tr><td colSpan="3">Sin datos</td>
-                        </tr>
-
-                        ) }
-                </tbody>
-            </table>
-        </div>
-    );
+const CrudTable = ({ data, setDatoEditar, deleteRegistro }) => {
+return (
+    <div class="ctabla">
+    <table>
+        <thread>
+        <tr>
+            <th>Usuario</th>
+            <th></th>
+            <th>contraseña</th>
+        </tr>
+        </thread>
+        <tbody>
+        {data.length > 0 ? (
+            data.map((el) => (
+            <CrudTableFila
+                key={el.id}
+                el={el}
+                setDatoEditar={setDatoEditar}
+                deleteRegistro={deleteRegistro}
+            />
+            ))
+        ) : (
+            <tr>
+            <td colSpan="3">Sin datos</td>
+            </tr>
+        )}
+        </tbody>
+    </table>
+    </div>
+);
 };
 
 export default CrudTable;
